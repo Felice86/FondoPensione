cab69a3014007b19ffaf0fbb650fee411d76f515
Felice86 <fefex86@gmail.com>
2015-11-25 09:35:19 +0100

    First commit - fix project

---
+++ FondoPensione/FondoPensione/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FondoPensione
+//
+//  Created by Felice Vimercati on 25/11/15.
+//  Copyright © 2015 ElpoEdizioni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FondoPensione/FondoPensione/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FondoPensione
+//
+//  Created by Felice Vimercati on 25/11/15.
+//  Copyright © 2015 ElpoEdizioni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
